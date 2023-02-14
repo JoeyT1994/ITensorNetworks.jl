170876743efbed20b5c710363455cfd915c2ae0d
Joseph Tindall <joeytindall94@googlemail.com>
2023-02-14 14:22:19 -0500

    Bug Fix

--- examples/belief_propagation/ImaginaryTimeGroundStates.jl
+++ examples/belief_propagation/ImaginaryTimeGroundStates.jl
@@ -6,10 +6,11 @@ using Statistics
 using ITensorNetworks:
   orthogonalize,
   norm_sqr,
-  construct_initial_mts,
   get_environment,
   rename_vertices_itn,
-  calculate_contraction
+  nested_graph_leaf_vertices,
+  compute_message_tensors,
+  neighbor_vertices
 using KaHyPar
 using Dictionaries
 using Compat
@@ -52,7 +53,7 @@ function electron_gates(s::IndsNetwork, params; Δβ=0.1, rev=true)
   gates = ITensor[]
   for e in edges(s)
     hj =
-      params["U"] * op("Nup", s[maybe_only(src(e))]) * op("Ndn", s[maybe_only(dst(e))]) -
+      params["U"] * op("Nup", s[maybe_only(src(e))]) * op("Ndn", s[maybe_only(dst(e))]) +
       params["t"] * op("Cdagup", s[maybe_only(src(e))]) * op("Cup", s[maybe_only(dst(e))]) +
       params["t"] * op("Cdagdn", s[maybe_only(src(e))]) * op("Cdn", s[maybe_only(dst(e))])
     Gj = exp(-Δβ * 0.5 * hj)
@@ -65,18 +66,43 @@ function electron_gates(s::IndsNetwork, params; Δβ=0.1, rev=true)
   return gates
 end
 
+function apply_gates(ψ::ITensorNetwork, gates, s::IndsNetwork; nvertices_per_partition = 1, maxdim)
 
-n = 4
+  ψ = copy(ψ)
+  for gate in gates
+    ψψ = ψ ⊗ prime(dag(ψ); sites=[])
+    vertex_groups = nested_graph_leaf_vertices(
+      partition(partition(ψψ, group(v -> v[1], vertices(ψψ))); nvertices_per_partition)
+    )
+    mts = compute_message_tensors(ψψ; vertex_groups=vertex_groups)
+    v⃗ = neighbor_vertices(ψ, gate)
+    envs = get_environment(ψψ, mts, [(v⃗[1], 1), (v⃗[1], 2), (v⃗[2], 1), (v⃗[2], 2)])
+    ψ = apply(gate,ψ;maxdim, normalize=true,envs,envisposdef=true)
+
+  end
+
+  return ψ
+end
+
+    
+
+
+n = 2
 dims = (n, n)
 g = named_grid(dims)
-s = siteinds("Electron", g)
+maxdim = 2
 
-#params = Dict([("Jx", -0.8), ("Jy", -0.0), ("Jz", 0.0), ("hx", 0.0), ("hy", 0.0), ("hz", 2.2)])
+# s = siteinds("Electron", g)
+# params = Dict([("U", 5.0), ("t", -1.0)])
+# gates = electron_gates(s, params)
 
-#gates = spin_gates(s, params)
+s = siteinds("S=1/2", g)
+params = Dict([("Jx", -0.8), ("Jy", -0.0), ("Jz", 0.0), ("hx", 0.0), ("hy", 0.0), ("hz", 2.2)])
+gates = spin_gates(s, params)
 
-params = Dict([("U", 5.0), ("t", -1.0)])
-gates = electron_gates(s, params)
+ψ = ITensorNetwork(s, v -> "↑")
+
+apply_gates(ψ, gates, s; maxdim)
 
 #@show length(gates)
 #@show length(edges(g))
