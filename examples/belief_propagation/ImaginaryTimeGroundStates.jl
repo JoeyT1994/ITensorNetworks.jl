c9bc4e966f27ae64952d960ae3df7288a17a78c2
Joseph Tindall <joeytindall94@googlemail.com>
2023-02-14 15:26:52 -0500

    Extra Code

--- examples/belief_propagation/ImaginaryTimeGroundStates.jl
+++ examples/belief_propagation/ImaginaryTimeGroundStates.jl
@@ -5,12 +5,12 @@ using ITensorNetworks
 using Statistics
 using ITensorNetworks:
   orthogonalize,
-  norm_sqr,
   get_environment,
   rename_vertices_itn,
   nested_graph_leaf_vertices,
   compute_message_tensors,
-  neighbor_vertices
+  neighbor_vertices,
+  contract_inner
 using KaHyPar
 using Dictionaries
 using Compat
@@ -20,6 +20,8 @@ using NPZ
 using NamedGraphs
 using SplitApplyCombine
 using CairoMakie
+
+include("ExactDMRGBackend.jl")
 
 maybe_only(x) = x
 maybe_only(x::Tuple{T}) where {T} = only(x)
@@ -39,8 +41,12 @@ function spin_gates(s::IndsNetwork, params; Δβ=0.1, rev=true)
       2 * (params["hx"]/n_dstv) * op("Sx", s[maybe_only(dst(e))]) * op("Id", s[maybe_only(src(e))]) +
       2 * (params["hy"]/n_dstv) * op("Sy", s[maybe_only(dst(e))]) * op("Id", s[maybe_only(src(e))]) +
       2 * (params["hz"]/n_dstv) * op("Sz", s[maybe_only(dst(e))]) * op("Id", s[maybe_only(src(e))])
-    Gj = exp(-Δβ * 0.5 * hj)
-    push!(gates, Gj)
+    if(Δβ != nothing)
+      Gj = exp(-Δβ * 0.5 * hj)
+      push!(gates, Gj)
+    else
+      push!(gates, hj)
+    end
   end
 
   if (rev)
@@ -56,8 +62,13 @@ function electron_gates(s::IndsNetwork, params; Δβ=0.1, rev=true)
       params["U"] * op("Nup", s[maybe_only(src(e))]) * op("Ndn", s[maybe_only(dst(e))]) +
       params["t"] * op("Cdagup", s[maybe_only(src(e))]) * op("Cup", s[maybe_only(dst(e))]) +
       params["t"] * op("Cdagdn", s[maybe_only(src(e))]) * op("Cdn", s[maybe_only(dst(e))])
-    Gj = exp(-Δβ * 0.5 * hj)
-    push!(gates, Gj)
+
+    if(Δβ != nothing)
+      Gj = exp(-Δβ * 0.5 * hj)
+      push!(gates, Gj)
+    else
+      push!(gates, hj)
+    end
   end
 
   if (rev)
@@ -66,17 +77,23 @@ function electron_gates(s::IndsNetwork, params; Δβ=0.1, rev=true)
   return gates
 end
 
-function apply_gates(ψ::ITensorNetwork, gates, s::IndsNetwork; nvertices_per_partition = 1, maxdim)
+function apply_gates(ψ::ITensorNetwork, gates, s::IndsNetwork; nvertices_per_partition = 1, maxdim, useenvs=  true)
 
   ψ = copy(ψ)
   for gate in gates
-    ψψ = ψ ⊗ prime(dag(ψ); sites=[])
-    vertex_groups = nested_graph_leaf_vertices(
-      partition(partition(ψψ, group(v -> v[1], vertices(ψψ))); nvertices_per_partition)
-    )
-    mts = compute_message_tensors(ψψ; vertex_groups=vertex_groups)
     v⃗ = neighbor_vertices(ψ, gate)
-    envs = get_environment(ψψ, mts, [(v⃗[1], 1), (v⃗[1], 2), (v⃗[2], 1), (v⃗[2], 2)])
+    ψ = ITensorNetworks.orthogonalize(ψ, v⃗[1])
+    if(useenvs)
+      ψψ = ψ ⊗ prime(dag(ψ); sites=[])
+      vertex_groups = nested_graph_leaf_vertices(
+        partition(partition(ψψ, group(v -> v[1], vertices(ψψ))); nvertices_per_partition)
+      )
+      mts = compute_message_tensors(ψψ; vertex_groups=vertex_groups)
+
+      envs = get_environment(ψψ, mts, [(v⃗[1], 1), (v⃗[1], 2), (v⃗[2], 1), (v⃗[2], 2)])
+    else
+      envs = ITensor[]
+    end
     ψ = apply(gate,ψ;maxdim, normalize=true,envs,envisposdef=true)
 
   end
@@ -84,25 +101,59 @@ function apply_gates(ψ::ITensorNetwork, gates, s::IndsNetwork; nvertices_per_pa
   return ψ
 end
 
+function calc_energy(ψ::ITensorNetwork, params, s, maxdim)
+  E = 0
+  ψ = copy(ψ)
+  Z = contract_inner(ψ, ψ)
+  gates = spin_gates(s, params; Δβ = nothing, rev = false)
+
+  for gate in gates
+    Oψ = apply(gate, ψ; maxdim = 4*maxdim)
+    E += contract_inner(ψ, Oψ)
+  end
+
+  return E/Z
+end
+
+
     
 
+function main()
+  n = 2
+  dims = (n, n, n)
+  g = named_grid(dims)
+  maxdim = 2
+  nsweeps = 30
 
-n = 2
-dims = (n, n)
-g = named_grid(dims)
-maxdim = 2
+  # s = siteinds("Electron", g)
+  # params = Dict([("U", 5.0), ("t", -1.0)])
+  # gates = electron_gates(s, params)
 
-# s = siteinds("Electron", g)
-# params = Dict([("U", 5.0), ("t", -1.0)])
-# gates = electron_gates(s, params)
+  s = siteinds("S=1/2", g)
+  params = Dict([("Jx", -0.8), ("Jy", -0.0), ("Jz", 0.0), ("hx", 0.0), ("hy", 0.0), ("hz", 2.2)])
+  gates = spin_gates(s, params)
 
-s = siteinds("S=1/2", g)
-params = Dict([("Jx", -0.8), ("Jy", -0.0), ("Jz", 0.0), ("hx", 0.0), ("hy", 0.0), ("hz", 2.2)])
-gates = spin_gates(s, params)
+  DMRG_backend(params, g, 100)
+  ψ = ITensorNetwork(s, v -> "↑")
+  ψSU = copy(ψ)
+  ψBP = copy(ψ)
 
-ψ = ITensorNetwork(s, v -> "↑")
+  for i = 1:nsweeps
+    println("On Sweep "*string(i))
+    ψSU = apply_gates(ψSU, gates, s; maxdim, useenvs = false)
+    ψBP = apply_gates(ψBP, gates, s; maxdim)
+  end
 
-apply_gates(ψ, gates, s; maxdim)
+  ESU = calc_energy(ψSU, params, s, maxdim)
+  EBP = calc_energy(ψBP, params, s, maxdim)
+  @show ESU
+  @show EBP
 
-#@show length(gates)
-#@show length(edges(g))
+
+
+
+
+end
+
+ITensors.disable_warn_order()
+main()
