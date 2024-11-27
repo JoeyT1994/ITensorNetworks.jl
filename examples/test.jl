6adc117c2b054b67819e22bea004296dd3d11e02
JoeyT1994 <JoeyT1994@users.noreply.github.com>
2024-11-27 01:48:08 +0000

    Format .jl files

--- examples/test.jl
+++ examples/test.jl
@@ -5,17 +5,17 @@ using NamedGraphs.NamedGraphGenerators: named_grid
 using NamedGraphs.GraphsExtensions: rem_vertex
 
 function filter_terms(H, verts)
-    H_new = OpSum()
-    for term in terms(H)
-        if isempty(filter(v -> v ∈ verts, sites(term)))
-            H_new += term
-        end
+  H_new = OpSum()
+  for term in terms(H)
+    if isempty(filter(v -> v ∈ verts, sites(term)))
+      H_new += term
     end
-    return H_new
+  end
+  return H_new
 end
 
-g = named_grid((8,1))
+g = named_grid((8, 1))
 s = siteinds("S=1/2", g)
 H = ising(s)
-H_mod = filter_terms(H, [(4,1)])
+H_mod = filter_terms(H, [(4, 1)])
 ttno = ttn(H_mod, s)
